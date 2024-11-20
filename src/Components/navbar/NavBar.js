c3636098e9ac6e9ce887f2e14c1a63fbbb6fca56
nasibpv <nasibnpv@gmail.com>
2024-11-20 22:35:36 +0530

    Channge Json data

--- src/Components/navbar/NavBar.js
+++ src/Components/navbar/NavBar.js
@@ -11,13 +11,13 @@ function NavBar() {
       <Container className='nav-container'>
         <Navbar.Brand href="/" className='Navbar-name pe-5 fw-bold '>PORTFOLIO</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav "  />
-        <Navbar.Collapse id="basic-navbar-nav m-0 p-0  ">
-          <Nav className="nav-all-link d-flex justify-content-end aline-items-start gap-3">
-            <Nav.Link className='nav-link text-light p-0' href="/My_Portfolio/">HOME</Nav.Link>
-            <ScrollLink to="about" spy={true} smooth={true} duration={500}>ABOUT</ScrollLink>         
-            <ScrollLink to="project" spy={true} smooth={true} duration={500}>PROJECT</ScrollLink>
-            <ScrollLink to="experience" spy={true} smooth={true} duration={500}>EXPERIENCE</ScrollLink>
-            <ScrollLink to="contact" spy={true} smooth={true} duration={500}>CONTACT</ScrollLink>
+        <Navbar.Collapse id="basic-navbar-nav m-0 p-0 d-flex justiffy-content-start  ">
+          <Nav className="nav-all-link d-flex justify-content-end aline-items-start gap-2 pt-1">
+            <Nav.Link   className='nav-link' href="/My_Portfolio/">HOME</Nav.Link>
+            <ScrollLink className='nav-link' to="about" spy={true} smooth={true} duration={500}>ABOUT</ScrollLink>         
+            <ScrollLink className='nav-link' to="project" spy={true} smooth={true} duration={500}>PROJECT</ScrollLink>
+            <ScrollLink className='nav-link' to="experience" spy={true} smooth={true} duration={500}>EXPERIENCE</ScrollLink>
+            <ScrollLink className='nav-link' to="contact" spy={true} smooth={true} duration={500}>CONTACT</ScrollLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
